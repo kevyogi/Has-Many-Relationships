60b2cd393931f346d48ec5e3370bb4b96c0e94ce
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-14 13:40:20 -1000

    finished query 9

--- joins.sql
+++ joins.sql
@@ -14,7 +14,7 @@
 -- WHERE p.users_id = 200;
 
 --4
--- SELECT *, u.username
+-- SELECT p.id, p.title, p.url, p.created_at, p.content, p.updated_at, u.username
 -- FROM posts p
 -- JOIN users u ON u.id = p.users_id
 -- WHERE u.first_name = 'Norene'
@@ -25,3 +25,31 @@
 -- FROM users u
 -- JOIN posts p ON p.users_id = u.id
 -- WHERE p.created_at > '2015-1-1'::date;
+
+--6
+-- SELECT p.title, p.content, u.username
+-- FROM posts p
+-- JOIN users u ON u.id = p.users_id
+-- WHERE u.created_at < '2015-1-1'::date;
+
+--7
+-- SELECT c.id, c.body, c.created_at, c.updated_at, p.title AS post_title
+-- FROM comments AS c, posts AS p
+-- WHERE p.id = c.posts_id;
+
+--8
+-- SELECT p.title AS post_title, p.url AS post_url, c.body AS comment_body
+-- FROM comments AS c, posts AS p
+-- WHERE p.created_at < '2015-1-1'::date
+-- AND p.id = c.posts_id;
+
+--9
+-- SELECT p.title AS post_title, p.url AS post_url, c.body AS comment_body
+-- FROM comments AS c, posts AS p
+-- WHERE p.created_at > '2015-1-1'::date
+-- AND p.id = c.posts_id;
+
+SELECT p.title AS post_title, p.url AS post_url, c.body AS comment_body
+FROM comments c
+JOIN posts p ON p.id = c.posts_id
+WHERE p.created_at > '2015-1-1'::date;
