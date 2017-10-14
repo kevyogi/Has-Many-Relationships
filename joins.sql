218cd3c90dd8c72b6ea2c01eb53bc72698e781a0
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-14 11:37:17 -1000

    finished query 4

--- joins.sql
+++ joins.sql
@@ -3,6 +3,19 @@
 -- FROM users;
 
 --2
-SELECT *
-FROM posts
-WHERE users_id = 100;
+-- SELECT *
+-- FROM posts
+-- WHERE users_id = 100;
+
+--3
+-- SELECT *, u.first_name, u.last_name
+-- FROM posts p
+-- JOIN users u ON u.id = p.users_id
+-- WHERE p.users_id = 200;
+
+--4
+SELECT *, u.username
+FROM posts p
+JOIN users u ON u.id = p.users_id
+WHERE u.first_name = 'Norene'
+AND u.last_name = 'Schmitt';
