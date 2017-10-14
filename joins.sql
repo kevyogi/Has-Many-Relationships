877797adccca74f3b3cd462c812d247b8a002806
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-14 11:48:19 -1000

    finished query 5

--- joins.sql
+++ joins.sql
@@ -14,8 +14,14 @@
 -- WHERE p.users_id = 200;
 
 --4
-SELECT *, u.username
-FROM posts p
-JOIN users u ON u.id = p.users_id
-WHERE u.first_name = 'Norene'
-AND u.last_name = 'Schmitt';
+-- SELECT *, u.username
+-- FROM posts p
+-- JOIN users u ON u.id = p.users_id
+-- WHERE u.first_name = 'Norene'
+-- AND u.last_name = 'Schmitt';
+
+--5
+-- SELECT username
+-- FROM users u
+-- JOIN posts p ON p.users_id = u.id
+-- WHERE p.created_at > '2015-1-1'::date;
